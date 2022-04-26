a0142338e3543a3556543f94e0324f9135b0a161
komgyu <38833796+komgyu@users.noreply.github.com>
2022-04-26 14:56:54 +0200

    Add files via upload

---
+++ scale.py
@@ -0,0 +1,14 @@
+import numpy as np
+import cv2
+import glob
+
+files = glob.glob('./original_data/original/*.png')
+i = 0 
+while i < len(files):
+	img = cv2.imread(files[i], 1)
+	img_half = cv2.resize(img, (480, 256), interpolation=cv2.INTER_AREA)
+	imgname = files[i].replace('./original_data/original\\','')
+	print(imgname)
+	cv2.imwrite('./original_data/scale/original/'+ str(imgname), img_half)
+	i += 1
+
